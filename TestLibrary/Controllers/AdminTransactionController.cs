e99454f5dac55f356c188db1d86d9de5eb1b7d32
Suratchanan Kraidech <benchbb01@hotmail.com>
2014-09-22 02:38:09 +0700

    Re-formatted class functional(Controller) - V
    Fully functional of AdminTransaction Module with fetch data feature.

--- TestLibrary/Controllers/AdminTransactionController.cs
+++ TestLibrary/Controllers/AdminTransactionController.cs
@@ -13,87 +13,56 @@ namespace TestLibrary.Controllers
     {
 
         LibraryContext db = new LibraryContext();
-
-        [Authorize]
-        public ActionResult Index()
+        private MemberTransactionViewer Check(BorrowEntry newentry)
         {
-            Session["LoginUser"] = HttpContext.User.Identity.Name;
-            if (HttpContext.User.Identity.Name.ToString().Substring(0, 2) != "A_")
-                return RedirectToAction("Index", "Account");
-            return View(db.BorrowList.ToList());
-        }
-
-        //Show viewtable if user click 'Check'
-        //To decide whether prefer request is OK?
-        [Authorize]
-        public ActionResult Borrow()
-        {
-            if (HttpContext.User.Identity.Name.ToString().Substring(0, 2) != "A_")
-                return RedirectToAction("Index", "Account");
-            return View();
-        }
-
-
-        [Authorize]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Borrow(BorrowEntry newentry,string operation)
-        {
-            ModelState.Remove("BookID");
-            TempData["UserID"] = newentry.UserID;
-            TempData["BookID"] = newentry.BookID;
-            if (ModelState.IsValid)
+            if (db.Members.Find(newentry.UserID) == null)
             {
-                if (operation == "Check")
-                {
-                    if (db.Members.Find(newentry.UserID) == null)
+                TempData["Notification"] = "No member that id's exists.";
+                return null;
+            }
+
+            MemberTransactionViewer viewer = new MemberTransactionViewer();
+            viewer.SetBorrowEntryViews(db.BorrowList.Where(entry => entry.UserID == newentry.UserID
+                && entry.ReturnDate == null).ToList());
+
+            List<RequestEntry> ReqList = db.RequestList.ToList();
+            List<RequestEntry> expireList = ReqList.Where(entry => entry.ExpireDate != null).ToList();
+
+            //Call list of requestlist of preferred member that expiredate field is null.
+            List<RequestEntry> WantedList = ReqList.Where(entry => entry.UserID == newentry.UserID
+                                            && entry.ExpireDate == null).ToList();
+            //Append wantedList with expiredate in range
+            WantedList.AddRange(expireList.Where(entry => entry.UserID == newentry.UserID
+                                                && entry.ExpireDate >= DateTime.Now.Date).ToList());
+
+            //Fetch requestList up-to-date by delete expire list.
+            expireList = expireList.Where(entry => entry.ExpireDate < DateTime.Now.Date).ToList();
+            if (expireList != null)
+            {
+                foreach (var item in expireList)
+                    item.RequestBook.BookStatus = Status.Available;
+
+                db.RequestList.RemoveRange(expireList);
+            }
+            db.SaveChanges();
+            viewer.SetRequestEntryViews(WantedList);
+            return viewer;
+        }
+
+
+        private MemberTransactionViewer Borrow(BorrowEntry newentry)
+        {
+            if (db.Members.Find(newentry.UserID) == null)
                     {
                         TempData["Notification"] = "No member that id's exists.";
-                        return View();
-                    }
-
-                    MemberTransactionViewer viewer = new MemberTransactionViewer();
-                    viewer.SetBorrowEntryViews(db.BorrowList.Where(entry => entry.UserID == newentry.UserID 
-                        && entry.ReturnDate == null).ToList());
-                    
-                    List<RequestEntry> ReqList = db.RequestList.ToList();
-                    List<RequestEntry> expireList = ReqList.Where(entry => entry.ExpireDate != null).ToList();
-                    
-                    //Call list of requestlist of preferred member that expiredate field is null.
-                    List<RequestEntry> WantedList = ReqList.Where(entry => entry.UserID == newentry.UserID 
-                                                    && entry.ExpireDate == null).ToList();
-                    //Append wantedList with expiredate in range
-                    WantedList.AddRange(expireList.Where(entry => entry.UserID == newentry.UserID 
-                                                        && entry.ExpireDate >= DateTime.Now.Date).ToList());
-
-                    //Fetch requestList up-to-date by delete expire list.
-                    expireList = expireList.Where(entry => entry.ExpireDate < DateTime.Now.Date).ToList();
-                    if (expireList != null) { 
-                    foreach (var item in expireList)
-                        item.RequestBook.BookStatus = Status.Available;
-
-                    db.RequestList.RemoveRange(expireList);
-                    }
-                    db.SaveChanges();
-                    viewer.SetRequestEntryViews(WantedList);
-                    return View(viewer);
-                }
-
-
-
-                else if(operation == "Submit")
-                {
-                    if (db.Members.Find(newentry.UserID) == null)
-                    {
-                        TempData["Notification"] = "No member that id's exists.";
-                        return View();
+                        return null;
                     }
 
                     if (db.BorrowList.Where(target => target.UserID == newentry.UserID
                         && target.ReturnDate == null).ToList().Count == 3)
                     {
                         TempData["Notification"] = "This member borrow exceed maximum allowed.";
-                        return View();
+                        return Check(newentry);
                     }
 
                     Book booktoborrow = db.Books.Find(newentry.BookID);
@@ -101,7 +70,7 @@ namespace TestLibrary.Controllers
                     if (booktoborrow == null)
                     {
                         TempData["Notification"] = "No book was found in database.";
-                        return View();
+                        return Check(newentry);
                     }
                     else if (booktoborrow.BookStatus == Status.Available)
                     {
@@ -112,12 +81,12 @@ namespace TestLibrary.Controllers
                         db.BorrowList.Add(newentry);
                         db.SaveChanges();
                         TempData["Notification"] = "OK";
-                        return View();
+                        return Check(newentry);
                     }
                     else if (booktoborrow.BookStatus == Status.Lost)
                     {
                         TempData["Notification"] = "This book is lost.";
-                        return View();
+                        return Check(newentry);
                     }
 
                     else if (booktoborrow.BookStatus == Status.Reserved)
@@ -133,12 +102,12 @@ namespace TestLibrary.Controllers
                             db.BorrowList.Add(newentry);
                             db.SaveChanges();
                             TempData["Notification"] = "Delete expire req.//OK.";
-                            return View();
+                            return Check(newentry);
                         }
                         else if (reqentry.UserID != newentry.UserID)
                         {
                             TempData["Notification"] = "This user has no permission to borrow the requested book by others.";
-                            return View();
+                            return Check(newentry);
                         }
                         else
                         {
@@ -150,38 +119,30 @@ namespace TestLibrary.Controllers
                             db.BorrowList.Add(newentry);
                             db.SaveChanges();
                             TempData["Notification"] = "User accept reserved book//OK";
-                            return View();
+                            return Check(newentry);
                         }
                     }
 
                     else
                     {
                         TempData["Notification"] = "This book is already borrowed.";
-                        return View();
+                        return Check(newentry);
                     }
-                }
-                else
-                {
-                    TempData["Notification"] = "Invalid operation.";
-                    return View();
-                }
-            }
-            else return View();
+               
         }
 
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        [HttpPost]
-        public ActionResult Return(int id)
+        private MemberTransactionViewer Return(BorrowEntry returnentry)
         {
-            BorrowEntry entry = db.BorrowList.Find(id);
+            BorrowEntry entry = db.BorrowList.Find(returnentry.ID);
             if (entry == null)
             {
                 TempData["Notification"] = "No borrow record found to do return.";
+                return null;
             }
             else if (entry.ReturnDate != null)
             {
                 TempData["Notification"] = "This book is already returned.";
+                return Check(entry);
             }
             else
             {
@@ -198,25 +159,64 @@ namespace TestLibrary.Controllers
                 db.Entry(entry).State = EntityState.Modified;
                 db.SaveChanges();
                 TempData["Notification"] = "Return successfully.";
+                return Check(entry);
             }
-            return RedirectToAction("Borrow");
+
         }
 
-        
 
-       /* [HttpPost]
         [Authorize]
-        public ActionResult test()
+        public ActionResult Index()
         {
-            return RedirectToAction("test2");
+            Session["LoginUser"] = HttpContext.User.Identity.Name;
+            if (HttpContext.User.Identity.Name.ToString().Substring(0, 2) != "A_")
+                return RedirectToAction("Index", "Account");
+            return View(db.BorrowList.ToList());
         }
 
+        //Show viewtable if user click 'Check'
+        //To decide whether prefer request is OK?
+        [Authorize]
+        public ActionResult Transaction()
+        {
+            if (HttpContext.User.Identity.Name.ToString().Substring(0, 2) != "A_")
+                return RedirectToAction("Index", "Account");
+            return View();
+        }
+
+
+        [Authorize]
         [HttpPost]
-        [Authorize]
-        public ActionResult test2()
+        [ValidateAntiForgeryToken]
+        public ActionResult Transaction(BorrowEntry newentry,string operation)
         {
-            return 
-        }*/
+            ModelState.Remove("BookID");
+            TempData["UserID"] = newentry.UserID;
+            TempData["BookID"] = (newentry.BookID == 0)?null:newentry.BookID.ToString();
+            if (ModelState.IsValid)
+            {
+                if (operation == "Check")
+                {
+                    return View(Check(newentry));
+                }
+
+                else if (operation == "Submit")
+                {
+                    return View(Borrow(newentry));
+                }
+                else if (operation == "Return")
+                {
+                    return View(Return(newentry));
+                }
+                else
+                {
+                    TempData["Notification"] = "Invalid operation.";
+                    return View();
+                }
+            }
+            else return View();
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
