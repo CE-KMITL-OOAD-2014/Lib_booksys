fbd477cbf1b23f2b5f5648fb71101a89f1657de2
Suratchanan Kraidech <benchbb01@hotmail.com>
2014-10-24 00:46:30 +0700

    API documentation - III

--- TestLibrary/Scripts/ApiScript.js
+++ TestLibrary/Scripts/ApiScript.js
@@ -5,7 +5,6 @@
     $("table.list").on("click", "tr.test button.view-btt", function () {
         view($(this).val());
     });
-
     $.getJSON("/api/BookQuery").done(function (data) {
         $.each(data, function (key, item) {
             $("table.list").append("<tr class=\"test\"><td>" + item.BookID + "</td><td>"
@@ -18,6 +17,8 @@
 });
 
 function viewBookDetail(id) {
+    if (id == "")
+        return null;
     var received_data;
     $.getJSON("/api/BookQuery/" + id)
         .done(function (data) {
@@ -100,59 +101,12 @@ function getBookByAllProperties(name,author,publisher,year) {
     return received_data;
 }
 
-
-
-
-function findBookByName() {
-    $.getJSON("/api/BookQuery/?name=" + $("input[name='bookname']").val())
-        .done(function (data) {
-            $("table.list").show();
-            $("table.list tr.test").remove();
-            $("p.noti-msg").text("");
-            $.each(data, function (key, item) {
-                $("table.list").append("<tr class=\"test\"><td>" + item.BookID + "</td><td>"
-                + item.BookName + "</td><td>" + item.Author + "</td>"
-                + "<td><button class=\"view-btt\" value=\"" + item.BookID + "\">View</button></td></tr>");
-            });
-        })
-        .fail(function (JqxCR, error, err) {
-            $("table.list").hide();
-            $("p.noti-msg").text(err);
-        });
-
-}
-
-function view(id) {
-    $.getJSON("/api/BookQuery/" + id)
-        .done(function (data) {
-            $("div.detail").children().remove();
-            $("div.detail").append("<p><b>" + data.BookID + " " + data.BookName + "<br>Detail:" + data.Detail + "<br>Year:" + data.Year + "</p>");
-        });
-}
-
-function findBook() {
-    var target = new Object();
-    target.BookName = $("input[name='bookname']").val();
-    target.Author = $("input[name='author']").val();
-    target.Publisher = $("input[name='publisher']").val();
-    target.Year = $("input[name='year']").val();
-
-    $.ajax({
-        type: "POST",
-        url: "/api/BookQuery/",
-        data: JSON.stringify(target),
-        contentType: "application/json"
-    }).done(function (data) {
-        $("p.noti-msg").text("");
-        $("table.list tr.test").remove();
-        $("table.list").show();
-        $.each(data, function (key, item) {
-            $("table.list").append("<tr class=\"test\"><td>" + item.BookID + "</td><td>"
-            + item.BookName + "</td><td>" + item.Author + "</td>"
-            + "<td><button class=\"view-btt\" value=\"" + item.BookID + "\">View</button></td></tr>");
-        });
-    }).fail(function (JqrdR, error, err) {
-        $("table.list").hide();
-        $("p.noti-msg").text(err);
-    });
+function getBookStatus(value) {
+    switch (value) {
+        case 0: return "Available";
+        case 1: return "Borrowed";
+        case 2: return "Reserved";
+        case 3: return "Lost";
+        default: return null;
+    }
 }
